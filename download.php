be4040dd62ed5cd67e5f446efadfac353d1da521
Forrest Fuqua <kingjrwr@gmail.com>
2017-05-31 23:38:04 -0500

    This is now working!

--- download.php
+++ download.php
@@ -18,12 +18,35 @@ $flash = substr($roomhtml["content"],$pos, 256);
 //extract data
 $explode = explode(";",(explode("'", $flash))[2]);
 // "roomid=13&amp;closed=1&amp;log=d27d7c57&amp;svr=chat-img01&amp;img=http%3A%2F%2Fchat-img01.pixiv.net%2F000%2F000%2F013%2Fd27d7c57%2Fimage_1137.png"
+$srv = str_replace([0 => "svr=", 1 => "&amp"], "", $explode[3]);
+$logid = str_replace([0 => "log=", 1 => "&amp"], "", $explode[2]);
 
-
-//$x = $x + 1;
-//file_put_contents("state", $x);
-//echo "wrote state $x";
+$api = downloadfile('http://chat.pixiv.net/api/capturepos.php?roomid=' . $x, 'http://chat.pixiv.net/');
+if($api["returncode"] != 200){echo "API returned {$api['returncode']}, sleeping 1s"; echo $api["headers"]; sleep(1); continue;}
+$apijson = json_decode($api["content"], TRUE);
+if(empty($apijson)){echo "API returned {$api['returncode']}, sleeping 1s"; echo $api["headers"]; sleep(1); continue;}
+file_put_contents("stor/$x/$logid.json", $api["content"]);
+$apicount = count($apijson);
+echo "Downloading $apicount parts" . PHP_EOL;
+$apicur = 0;
+while (list($apikey, $apirow) = each($apijson)) {
+$apicur = $apicur + 1;
+//split room number
+$splitid = str_pad($x, 9, 0, STR_PAD_LEFT);
+echo "New ID: $splitid" . PHP_EOL;
+$splitid = str_split($splitid, 3);
+$getamfid = "$apicur" . "-" . "$apirow";
+echo "Downloading $getamfid" . PHP_EOL;
+$getamf = downloadfile('http://'.$srv.'.pixiv.net/'.$splitid[0].'/'.$splitid[1].'/'.$splitid[2].'/'.$logid.'/'.$getamfid.'.amf', 'http://chat.pixiv.net/');
+if($getamf["returncode"] != 200){echo "AMF returned {$getamf['returncode']}, sleeping 1s"; echo $getamf["headers"]; sleep(1); $apicur = 0; reset($apijson); continue;}
+file_put_contents("stor/$x/$getamfid.amf", $getamf["content"]);
+$apicur = $apirow;
 }
+$x = $x + 1;
+file_put_contents("state", $x);
+echo "wrote state $x";
+}
+
 
 
 function downloadfile($url, $referer){
